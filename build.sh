26bdb6b8de54eaa063f3a8589341111262068c65
rara64 <60274052+rara64@users.noreply.github.com>
2024-12-21 12:45:16 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -102,8 +102,12 @@ deps() {
      	find . -type f \( -name "*.cpp" -o -name "*.hpp" \) -print0 | while IFS= read -r -d $'\0' file; do
         if grep -q 'gettimeofday.*NULL);' "$file"; then
             echo "Patching $file"
-            sed -i 's/gettimeofday(\([^,]*\), NULL);/struct timespec pruTs;\nclock_gettime(CLOCK_MONOTONIC, \&pruTs);\npruTs.tv_sec += 1734736016;\nTIMESPEC_TO_TIMEVAL(\1, \&pruTs);/g' "$file"
+            sed -i 's/gettimeofday(\([^,]*\), NULL);/struct timespec pruTs;\nclock_gettime(CLOCK_MONOTONIC, \&pruTs);\nTIMESPEC_TO_TIMEVAL(\1, \&pruTs);/g' "$file"
         fi
+	if [[basename($file) == "RTPServer.cpp"]]; then
+ 	   echo "ADDING DEBUG $file"
+	   sed -i '1i#define DEBUG 1\n' "$file"
+ 	fi
     	done
 
 	if [[ -f Makefile ]]; then
