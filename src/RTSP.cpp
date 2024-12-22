6081cb5a23e8d1b403da45b9511fc83ce69a8291
rara64 <60274052+rara64@users.noreply.github.com>
2024-12-22 23:30:06 +0100

    Update RTSP.cpp

--- src/RTSP.cpp
+++ src/RTSP.cpp
@@ -120,7 +120,7 @@ void RTSP::start()
     }
 
     global_rtsp_thread_signal = 0;
-    env->taskScheduler().doEventLoop(&global_rtsp_thread_signal);
+    env->taskScheduler().doEventLoop(&global_rtsp_thread_signal, 1);
 
     // Clean up VPS if it was allocated
     /*
