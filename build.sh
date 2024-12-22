e791e8fef4b217b6f49f5bb5f7d129402510dab4
rara64 <60274052+rara64@users.noreply.github.com>
2024-12-22 21:20:37 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -112,28 +112,29 @@ deps() {
   	if [[ $(basename "$file") == "DelayQueue.cpp" ]]; then
 sed -i '1s/^/#include <cstdio>\n/' "$file"
 
-sed -i "/_EventTime timeNow = TimeNow();/a \\
+sed -i "/_EventTime timeNow = TimeNow();/a \
         fprintf(stderr, \"[sync] timeNow: %ld.%06ld\\n\", timeNow.tv_sec, timeNow.tv_usec);" "$file"
 
-sed -i "/fLastSyncTime = timeNow;/a \\
-        fprintf(stderr, \"[sync] fLastSyncTime: %ld.%06ld, timeSinceLastSync: %ld.%06ld\\n\", \\
-                fLastSyncTime.tv_sec, fLastSyncTime.tv_usec, \\
-                timeSinceLastSync.tv_sec, timeSinceLastSync.tv_usec);" "$file"
+sed -i "/if \(timeNow < fLastSyncTime\) {/a \
+        fprintf(stderr, \"[sync] WARNING: System clock went back in time!\\n\");" "$file"
 
-sed -i "/DelayQueueEntry\* curEntry = head();/a \\
+sed -i "/fLastSyncTime = timeNow;/a \
+        fprintf(stderr, \"[sync] fLastSyncTime: %ld.%06ld, timeSinceLastSync: %ld.%06ld\\n\", fLastSyncTime.tv_sec, fLastSyncTime.tv_usec, timeSinceLastSync.tv_sec, timeSinceLastSync.tv_usec);" "$file"
+
+sed -i "/DelayQueueEntry\* curEntry = head();/a \
         int entryIndex = 0;" "$file"
 
-sed -i "/while (.*timeSinceLastSync >= curEntry->fDeltaTimeRemaining)/a \\
-            fprintf(stderr, \"[sync] Entry %d: fDeltaTimeRemaining: %ld.%06ld\\n\", \\
-                    entryIndex, curEntry->fDeltaTimeRemaining.tv_sec, curEntry->fDeltaTimeRemaining.tv_usec);\\
-            fprintf(stderr, \"[sync] TRIGGERING TASK for entry %d\\n\", entryIndex);" "$file"
+sed -i "/while (.*timeSinceLastSync >= curEntry->fDeltaTimeRemaining)/a \
+        fprintf(stderr, \"[sync] Entry %d: fDeltaTimeRemaining: %ld.%06ld\\n\", entryIndex, curEntry->fDeltaTimeRemaining.tv_sec, curEntry->fDeltaTimeRemaining.tv_usec);\\\
+        fprintf(stderr, \"[sync] TRIGGERING TASK for entry %d\\n\", entryIndex);" "$file"
 
-sed -i "/curEntry = curEntry->fNext;/a \\
-            entryIndex++;" "$file"
+sed -i "/curEntry = curEntry->fNext;/a \
+        entryIndex++;" "$file"
 
-sed -i "/curEntry->fDeltaTimeRemaining -= timeSinceLastSync;/a \\
-            fprintf(stderr, \"[sync] Entry %d: fDeltaTimeRemaining after adjustment: %ld.%06ld\\n\", \\
-                    entryIndex, curEntry->fDeltaTimeRemaining.tv_sec, curEntry->fDeltaTimeRemaining.tv_usec);" "$file"
+sed -i "/curEntry->fDeltaTimeRemaining -= timeSinceLastSync;/a \
+        fprintf(stderr, \"[sync] Entry %d: fDeltaTimeRemaining after adjustment: %ld.%06ld\\n\", entryIndex, curEntry->fDeltaTimeRemaining.tv_sec, curEntry->fDeltaTimeRemaining.tv_usec);" "$file"
+
+ cat "$file"
       
    	fi
     	done
