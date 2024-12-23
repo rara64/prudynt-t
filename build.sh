5c6978ae56d5859d817899475b29f277425a6274
rara64 <60274052+rara64@users.noreply.github.com>
2024-12-23 13:02:53 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -112,7 +112,7 @@ deps() {
     	if [[ $(basename "$file") == "GenericMediaServer.cpp" ]]; then
  	   echo "ADDING DEBUG $file"
 	   sed -i '1i#define DEBUG 1\n' "$file"
-	   sed -i "s/\(fOurServer.fReclamationSeconds\)\(\*1000000\)/\1*2000000/" "$file"
+	   #sed -i "s/\(fOurServer.fReclamationSeconds\)\(\*1000000\)/\1*2000000/" "$file"
     	fi
      	if [[ $(basename "$file") == "DelayQueue.cpp" ]]; then
    	   sed -i '1s/^/#include <cstdio>\n/' "$file"
